8682c17d403b308e0957494be90c2cf78f17ef2c
squinky86 <squinky86@gmail.com>
2021-04-02 10:01:59 -0500

    coming soon

---
+++ html/index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Software Assurance Tips</title>
+	<meta name="description" content="Software Assurance Tips" />
+</head>
+<body>
+	<h1>Coming Soon</h1>
+</body>
+</html>
